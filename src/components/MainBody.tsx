c9476cf2abac1c29359210fe77d06858cd205326
Max Zakharzhevskiy <maxzz@msn.com>
2022-04-04 16:36:57 -0700

    WIP. HueSlider. initial implementation

--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import { useState } from 'react';
+import './hue-slider.scss';
+
+function HueSlider() {
+    const [color, setColor] = useState(40);
+    return (
+        <input
+            className="hue-slider"
+            type="range"
+            min="0"
+            max="360"
+            value={color}
+            onChange={(event) => {
+                console.log('vvv', +event.target.value);
+                
+                setColor(+event.target.value);
+            }}
+        />
+    );
+}
 
 export function MainBody() {
     return (
         <div className="text-primary-100">
             Ground zero
+            <HueSlider />
         </div>
     );
 }
