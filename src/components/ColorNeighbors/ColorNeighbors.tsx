54ee209ca51179f8375894f288fc31b728d501aa
Max Zakharzhevskiy <maxzz@msn.com>
2022-04-06 01:40:19 -0700

    WIP. ColorNeighbors. fix axis location

--- src/components/ColorNeighbors/ColorNeighbors.tsx
+++ src/components/ColorNeighbors/ColorNeighbors.tsx
@@ -8,24 +8,26 @@ export function ColorNeighbors() {
     const [colorsGroups] = useAtom(colorGroupsAtom);
     const setGlobalColor = useUpdateAtom(globalColorAtom);
     return (
-        <div className="place-self-center relative w-1/2 max-w-[42rem] aspect-square grid border border-slate-200 shadow-md">
-            {colorsGroups.map((group, groupIdx) => (
-                <div className="grid grid-flow-col" key={groupIdx}>
-                    {group.map((color, colorIdx) => (
-                        <button
-                            className="hover:scale-[1.02] hover:shadow-md hover:border-slate-500 hover:border transition-transform"
-                            style={{
-                                backgroundColor: color.name,
-                                color: color.type === 'light' ? 'black' : 'white'
-                            }}
-                            key={colorIdx}
-                            onClick={() => setGlobalColor(color)}
-                        >
-                            {color.name}
-                        </button>
-                    ))}
-                </div >
-            ))}
+        <div className="place-self-center relative w-1/2 max-w-[42rem] aspect-square">
+            <div className="w-full h-full grid border border-slate-200 shadow-md">
+                {colorsGroups.map((group, groupIdx) => (
+                    <div className="grid grid-flow-col" key={groupIdx}>
+                        {group.map((color, colorIdx) => (
+                            <button
+                                className="hover:scale-[1.02] hover:shadow-md hover:border-slate-500 hover:border transition-transform"
+                                style={{
+                                    backgroundColor: color.name,
+                                    color: color.type === 'light' ? 'black' : 'white'
+                                }}
+                                key={colorIdx}
+                                onClick={() => setGlobalColor(color)}
+                            >
+                                {color.name}
+                            </button>
+                        ))}
+                    </div >
+                ))}
+            </div>
             <div className="axis axis-lightness">lightness</div>
             <div className="axis axis-saturation">saturation</div>
         </div>
