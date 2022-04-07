85ba87b48e1046d42dcd57b8160ea02576893219
Max Zakharzhevskiy <maxzz@msn.com>
2022-04-06 21:11:52 -0700

    WIP. Header. simplify look

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 export function Header() {
     return (
-        <div className="px-3 py-2 text-lg flex items-center text-purple-300 bg-purple-100 uppercase scale-x-50">
+        <div
+            className="px-3 py-2 text-lg flex items-center text-purple-300 uppercase scale-x-50"
+            style={{textShadow: '1px 1px red'}}
+        >
             Color Names
         </div>
     );
