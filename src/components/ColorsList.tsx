eafb38162d95b9744bc30e3792941bfe7eec29f7
Max Zakharzhevskiy <maxzz@msn.com>
2022-04-08 16:51:12 -0700

    WIP. colors. add colorList sort controls

--- src/components/ColorsList.tsx
+++ src/components/ColorsList.tsx
@@ -1,16 +1,26 @@
-import { useAtomValue } from 'jotai';
+import { useAtom, useAtomValue } from 'jotai';
 import React from 'react';
-import { colorListAtom } from '../store/store';
+import { colorListAtom, colorListSortByAtom } from '../store/store';
+import { SortBy } from '../utils/colors';
 
 export function ColorsList() {
     const colorList = useAtomValue(colorListAtom);
+    const [sortBy, setSortBy] = useAtom(colorListSortByAtom);
     return (
         <div className="">
-            <div>ColorsList</div>
+            <div className="">
+                <div className="">ColorsList</div>
+                <ul className="flex items-center space-x-2">
+                    <label><input className="mr-1" type="radio" value={SortBy.none} name="sortby" onChange={(event) => setSortBy(+event.target.value)} />none</label>
+                    <label><input className="mr-1" type="radio" value={SortBy.name} name="sortby" onChange={(event) => setSortBy(+event.target.value)} />name</label>
+                    <label><input className="mr-1" type="radio" value={SortBy.rgb} name="sortby" onChange={(event) => setSortBy(+event.target.value)} />rgb</label>
+                    <label><input className="mr-1" type="radio" value={SortBy.hsl} name="sortby" onChange={(event) => setSortBy(+event.target.value)} />hsl</label>
+                </ul>
+            </div>
             {colorList.map((color) => (
                 <div
                     className=""
-                    style={{backgroundColor: color.name}}
+                    style={{ backgroundColor: color.name }}
                     key={color.name}
                 >{color.name}</div>
             ))}
