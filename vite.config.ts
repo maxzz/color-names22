6934825d1cafd5907b97e29e6c41e05ed4a4f47b
Max Zakharzhevskiy <maxzz@msn.com>
2022-04-08 18:45:06 -0700

    WIP. fonts. add sources path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
+import path from 'node:path';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default ({ command }) => {
     return defineConfig({
         base: '',
-        plugins: [react()]
+        plugins: [react()],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            },
+        },
+        //assetsInclude: ['./src/**/*.woff2'],
     });
 };
