6cb556a4c82732f5cdaecd517eb3025e9a503e71
Max Zakharzhevskiy <maxzz@msn.com>
2022-04-11 15:15:27 -0700

    WIP. UI. add radio and checkbox switches

--- src/components/UI/UISwitch.tsx
+++ src/components/UI/UISwitch.tsx
@@ -7,10 +7,21 @@ console.log('styles', styles);
 export function UISwitch() {
     const [on, setOn] = useState(false);
     const label = "Name";
-    return (
+    return (<>
         <label className="flex items-center space-x-2 cursor-pointer select-none">
             <input type="checkbox" className={`${styles.switch} cursor-pointer`} />
             <span>{label}</span>
         </label>
+
+        <label className="flex items-center space-x-2 cursor-pointer select-none">
+            <input type="checkbox" className={`${styles.radio} cursor-pointer`} />
+            <span>{label}</span>
+        </label>
+
+        <label className="flex items-center space-x-2 cursor-pointer select-none">
+            <input type="checkbox" className={`${styles.checkbox} cursor-pointer`} />
+            <span>{label}</span>
+        </label>
+    </>
     );
 }
