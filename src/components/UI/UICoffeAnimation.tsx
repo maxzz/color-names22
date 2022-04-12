3f77b561afa05a71754a4ec30316815fc2a95b45
Max Zakharzhevskiy <maxzz@msn.com>
2022-04-12 14:33:52 -0700

    WIP. add UICoffeAnimation animated button

---
+++ src/components/UI/UICoffeAnimation.tsx
@@ -0,0 +1,11 @@
+import React from 'react'; //https://www.ctrl.blog/entry/apple-music-nullbytes.html
+import styles from './UICoffeAnimation.module.scss';
+
+console.log(styles);
+
+
+export function UICoffeAnimation() {
+    return (
+        <div className={`${styles.bttn}`}>By Me A Coffe</div>
+    );
+}
