1752acc9223510a7f34104204e899bfc3fad67e4
Max Zakharzhevskiy <maxzz@msn.com>
2022-05-23 02:50:00 -0700

    WIP. ColorInfoPanel. add copy notification. need clean up

--- src/components/sections/ColorsGroup/ColorInfoPanel.tsx
+++ src/components/sections/ColorsGroup/ColorInfoPanel.tsx
@@ -4,6 +4,7 @@ import { viewHueAtoms } from '../../../store/store';
 import { formatHSL, formatRGB } from '../../../utils/colors';
 import { classNames } from '../../../utils/classnames';
 import { IconClipboard } from '../../UI/UIIcons';
+import { a, easings, useTransition } from '@react-spring/web';
 
 function HueInfo() {
     const hue = useAtomValue(viewHueAtoms.hueAtom);
@@ -33,25 +34,79 @@ function ColorPreview() {
     );
 }
 
+function Mount({ show, setShow, children }: { show: boolean; setShow: (v: boolean) => void; } & React.HTMLAttributes<HTMLDivElement>) {
+    const transitions = useTransition(Number(show), {
+        // const transitions = useTransition(show, {
+        from: { x: 0, opacity: 0, },
+        enter: { x: 0, opacity: 1, config: { duration: 500, easing: easings.easeOutCubic }, },
+        leave: { x: 200, opacity: 0, config: { duration: 2400, easing: easings.easeOutQuad }, /* onRest: () => console.log('done') */ },
+        //exitBeforeEnter: true,
+
+        // onRest: (result, ctrl, item) => {
+        //     //console.log('done', finished);
+        //     console.log('done show= ', show, result, ctrl, item);
+
+        //     show && setShow(false);
+        // },
+
+        onRest: {
+            opacity: (result, ctrl, item) => {
+                //console.log('done', finished);
+                if (show) {
+                    console.log('done opacity show= ', show, result, ctrl, item);
+
+                    show && setShow(false);
+                }
+            }
+        },
+
+        // onRest: () => setShow(false),
+        //config: { duration: 200, },
+    });
+    return transitions((styles, item) => item
+        ? <a.div style={styles}>
+            {children}
+        </a.div>
+        : null
+    );
+}
+
+function CopyNotice() {
+    return (
+        <div className="">Copied</div>
+    );
+}
+
 function ValueWithCopy({ name, label }: { name: string; label: string; }) {
     const [focus, setFocus] = useState(false);
+    const [showNotice, setShowNotice] = useState(false);
     return (<>
         <div className="py-0.5 select-none">{name}</div>
-        <div onPointerEnter={() => setFocus(true)} onPointerLeave={() => setFocus(false)}
+        <div
+            className="flex items-center space-x-2"
+            onPointerEnter={() => setFocus(true)}
+            onPointerLeave={() => setFocus(false)}
             onClick={async () => {
                 await navigator.clipboard.writeText(label);
+                setShowNotice(true);
                 //toastSucceeded('Link copied to clipboard');
             }}
         >
-            <div className={classNames(
-                "inline-flex items-center space-x-1 cursor-pointer",
-                focus ? "px-1 py-0.5 bg-slate-100 outline-slate-500 outline-1 outline rounded shadow active:scale-[.97]" : "px-1 py-0.5"
-            )}>
+            <div
+                className={classNames(
+                    "inline-flex items-center cursor-pointer",
+                    focus ? "px-1 py-0.5 bg-slate-100 outline-slate-500 outline-1 outline rounded shadow active:scale-[.97]" : "px-1 py-0.5"
+                )}
+            >
                 <div className="">{label}</div>
-                <div className={classNames(focus ? "visible" : "invisible")}>
+                <div className={classNames("ml-1", focus ? "visible" : "invisible")}>
                     <IconClipboard className="w-4 h-4 text-slate-500" />
                 </div>
             </div>
+
+            {/* <Mount show={showNotice} setShow={setShowNotice} ><CopyNotice /></Mount> */}
+            <Mount show={showNotice} setShow={setShowNotice} >{showNotice && <CopyNotice />}</Mount>
+            {/* {showNotice && <CopyNotice />} */}
         </div>
     </>);
 }
@@ -62,9 +117,9 @@ function ColorInfo() {
         {color &&
             <div className="grid grid-cols-[auto,1fr] gap-x-2 gap-y-0.5">
                 <ValueWithCopy name={"Name"} label={color.name} />
-                <ValueWithCopy name={"Hex"} label={color.hex} />
+                {/* <ValueWithCopy name={"Hex"} label={color.hex} />
                 <ValueWithCopy name={"RGB"} label={formatRGB(color.rgb)} />
-                <ValueWithCopy name={"HSL"} label={formatHSL(color.hsl)} />
+                <ValueWithCopy name={"HSL"} label={formatHSL(color.hsl)} /> */}
             </div>
         }
     </>);
