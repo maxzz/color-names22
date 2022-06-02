3829a1095a0b2bd0ac6c574de9d309689ee34782
Max Zakharzhevskiy <maxzz@msn.com>
2022-06-02 16:37:47 -0700

    WIP. app. problem G: 'css flex align scroll'. another fix. but scrollbars always on

--- src/components/sections/Section3_Tailwind/Section3_Tailwind.tsx
+++ src/components/sections/Section3_Tailwind/Section3_Tailwind.tsx
@@ -5,15 +5,16 @@ import { TwColorInfo } from './TwColorInfo';
 
 export function Section3_Tailwind({ className }: React.HTMLAttributes<HTMLUListElement>) {
     return (
-        <div className="h-full overflow-hidden">
-            <div className={classNames("h-full flex flex-col bg-primary-100 overflow-overlay", className)}>
+        
+            <div className={classNames("h-full flex flex-col bg-primary-100 ", className)}>
                 <div className="bg-primary-200">
                     <TwColorInfo className="max-w-3xl mx-auto" />
                 </div>
-                <div className="flex-1 ">
+                <div className="flex-1 overflow-hidden">
                     {/* overflow-hidden */}
-                    <div className="h-full ">
+                    <div className="h-full overflow-overlay">
                         {/* overflow-overlay */}
+                        <div className="h-1"></div>
                         <div className={classNames("h-full p-8 flex flex-col items-center justify-center")}>
                             <div className="px-3 py-4 text-xs font-bold bg-primary-200 border-primary-300 border rounded shadow-md">
                                 <TwColorsView />
@@ -22,6 +23,6 @@ export function Section3_Tailwind({ className }: React.HTMLAttributes<HTMLUListE
                     </div>
                 </div>
             </div>
-        </div>
+        
     );
 }
