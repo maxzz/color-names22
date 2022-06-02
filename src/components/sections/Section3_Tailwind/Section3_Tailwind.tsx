508a5f1ee043a614b28eec5f880f04df292cacce
Max Zakharzhevskiy <maxzz@msn.com>
2022-06-02 16:51:35 -0700

    WIP. app. fix overflow problem w/ hooks

--- src/components/sections/Section3_Tailwind/Section3_Tailwind.tsx
+++ src/components/sections/Section3_Tailwind/Section3_Tailwind.tsx
@@ -2,27 +2,23 @@ import React from 'react';
 import { classNames } from '../../../utils/classnames';
 import { TwColorsView } from './TwColorsView';
 import { TwColorInfo } from './TwColorInfo';
+import useMeasure from 'react-use-measure';
 
 export function Section3_Tailwind({ className }: React.HTMLAttributes<HTMLUListElement>) {
+    const [refParent, parent] = useMeasure();
+    const [refChild, child] = useMeasure();
+    const noOverflow = parent.height > child.height;
     return (
-        
-            <div className={classNames("h-full flex flex-col bg-primary-100 ", className)}>
-                <div className="bg-primary-200">
-                    <TwColorInfo className="max-w-3xl mx-auto" />
-                </div>
-                <div className="flex-1 overflow-hidden">
-                    {/* overflow-hidden */}
-                    <div className="h-full overflow-overlay">
-                        {/* overflow-overlay */}
-                        <div className="h-1"></div>
-                        <div className={classNames("h-full p-8 flex flex-col items-center justify-center")}>
-                            <div className="px-3 py-4 text-xs font-bold bg-primary-200 border-primary-300 border rounded shadow-md">
-                                <TwColorsView />
-                            </div>
-                        </div>
-                    </div>
+        <div className={classNames("h-full flex flex-col bg-primary-100 overflow-hidden", className)}>
+            <div className="bg-primary-200">
+                <TwColorInfo className="max-w-3xl mx-auto" />
+            </div>
+
+            <div ref={refParent} className={classNames("flex-1 p-8 flex flex-col items-center overflow-overlay", noOverflow && "justify-center")}>
+                <div ref={refChild} className="px-3 py-4 text-xs font-bold bg-primary-200 border-primary-300 border rounded shadow-md">
+                    <TwColorsView />
                 </div>
             </div>
-        
+        </div>
     );
 }
