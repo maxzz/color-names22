01b2b6164622e5eea60aafb3ecb73765c22e305d
Max Zakharzhevskiy <maxzz@msn.com>
2022-06-02 16:30:42 -0700

    WIP. app. problem G: 'css flex align scroll'. another possible fix

--- src/components/sections/Section3_Tailwind/Section3_Tailwind.tsx
+++ src/components/sections/Section3_Tailwind/Section3_Tailwind.tsx
@@ -5,16 +5,19 @@ import { TwColorInfo } from './TwColorInfo';
 
 export function Section3_Tailwind({ className }: React.HTMLAttributes<HTMLUListElement>) {
     return (
-        <div className={classNames("h-full flex flex-col bg-primary-100", className)}>
-            <div className="bg-primary-200">
-                <TwColorInfo className="max-w-3xl mx-auto" />
-            </div>
-
-            <div className="flex-1 overflow-hidden">
-                <div className="h-full overflow-overlay">
-                    <div className={classNames("p-8 flex flex-col items-center")}>
-                        <div className="px-3 py-4 text-xs font-bold bg-primary-200 border-primary-300 border rounded shadow-md">
-                            <TwColorsView />
+        <div className="h-full overflow-hidden">
+            <div className={classNames("h-full flex flex-col bg-primary-100 overflow-overlay", className)}>
+                <div className="bg-primary-200">
+                    <TwColorInfo className="max-w-3xl mx-auto" />
+                </div>
+                <div className="flex-1 ">
+                    {/* overflow-hidden */}
+                    <div className="h-full ">
+                        {/* overflow-overlay */}
+                        <div className={classNames("h-full p-8 flex flex-col items-center justify-center")}>
+                            <div className="px-3 py-4 text-xs font-bold bg-primary-200 border-primary-300 border rounded shadow-md">
+                                <TwColorsView />
+                            </div>
                         </div>
                     </div>
                 </div>
