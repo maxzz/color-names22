dc5ec77e73b0cf8084426734dbe71d0fe6e0253f
Max Zakharzhevskiy <maxzz@msn.com>
2022-06-01 20:02:47 -0700

    WIP. Section1_ColorsByHue. position CopyNotice

--- src/components/sections/Section1_ColorsByHue/ColorInfoPanel.tsx
+++ src/components/sections/Section1_ColorsByHue/ColorInfoPanel.tsx
@@ -5,12 +5,24 @@ import { a, easings, useTransition } from '@react-spring/web';
 import { classNames } from '@/utils/classnames';
 import { formatHSL, formatRGB } from '@/utils/colors';
 import { MonoSwitch } from '@/components/UI/UIMonoSwitch';
-import { CopyNotice, ValueView, ValueWithCopy } from '@/components/UI/ValueWithCopy';
+import { ValueView, ValueWithCopy } from '@/components/UI/ValueWithCopy';
 import { HueSlider } from './HueSlider/HueSlider';
+
+const copyHueNoticeTextShadow = {
+    textShadow: '#98989887 1px 1px, #4141412e -1px -1px 0px'
+};
+
+function CopyHueNotice() {
+    return (
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <div className="px-2 py-px text-sm bg-green-500 text-green-900 border-green-700 border rounded" style={copyHueNoticeTextShadow}>Copied</div>
+        </div>
+    );
+}
 
 function SingleValue({ copyValue }: { copyValue: string; }) {
     return (<>
-        <ValueWithCopy copyValue={copyValue} copyNotice={<CopyNotice />}>
+        <ValueWithCopy copyValue={copyValue} copyNotice={<CopyHueNotice />}>
             <ValueView />
         </ValueWithCopy>
     </>);
@@ -21,13 +33,14 @@ function ColorPreview() {
     const borderColor = () => color ? color.dark ? 'white' : 'black' : 'transparent';
     return (
         <div
-            className="flex-1 p-1 w-24 h-24 border-primary-400 border rounded flex items-end"
+            className="relative flex-1 p-1 w-24 h-24 border-primary-400 border rounded flex items-end"
             style={{
                 backgroundColor: `${color ? color.hex : 'transparent'}`,
                 color: borderColor(),
                 borderColor: borderColor()
             }}
         >
+            {/* <CopyHueNotice /> */}
             {color &&
                 <div className="flex flex-col text-xs">
                     <SingleValue copyValue={color.name} />
