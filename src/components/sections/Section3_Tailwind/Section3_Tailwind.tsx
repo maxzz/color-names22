ecc17f851891c00365ee4624976cf1dbc29a0ce8
Max Zakharzhevskiy <maxzz@msn.com>
2022-06-02 16:56:35 -0700

    WIP. app. fix overflow problem w/ hooks rerenders

--- src/components/sections/Section3_Tailwind/Section3_Tailwind.tsx
+++ src/components/sections/Section3_Tailwind/Section3_Tailwind.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { classNames } from '../../../utils/classnames';
 import { TwColorsView } from './TwColorsView';
 import { TwColorInfo } from './TwColorInfo';
 import useMeasure from 'react-use-measure';
 
-export function Section3_Tailwind({ className }: React.HTMLAttributes<HTMLUListElement>) {
+function VerticalOverflowFix({ children }: { children: ReactNode; }) {
     const [refParent, parent] = useMeasure();
     const [refChild, child] = useMeasure();
     const noOverflow = parent.height > child.height;
+    return (
+        <div ref={refParent} className={classNames("flex-1 p-8 flex flex-col items-center overflow-overlay", noOverflow && "justify-center")}>
+            <div ref={refChild} className="px-3 py-4 text-xs font-bold bg-primary-200 border-primary-300 border rounded shadow-md">
+                {children}
+            </div>
+        </div>
+    );
+}
+
+export function Section3_Tailwind({ className }: React.HTMLAttributes<HTMLUListElement>) {
     return (
         <div className={classNames("h-full flex flex-col bg-primary-100 overflow-hidden", className)}>
             <div className="bg-primary-200">
                 <TwColorInfo className="max-w-3xl mx-auto" />
             </div>
 
-            <div ref={refParent} className={classNames("flex-1 p-8 flex flex-col items-center overflow-overlay", noOverflow && "justify-center")}>
-                <div ref={refChild} className="px-3 py-4 text-xs font-bold bg-primary-200 border-primary-300 border rounded shadow-md">
-                    <TwColorsView />
-                </div>
-            </div>
+            <VerticalOverflowFix>
+                <TwColorsView />
+            </VerticalOverflowFix>
         </div>
     );
 }
