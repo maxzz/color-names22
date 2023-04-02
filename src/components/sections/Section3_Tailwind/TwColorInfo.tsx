698016a9293858b8bb560caea132eef55df87f71
Max Zakharzhevskiy <maxzz@msn.com>
2023-04-01 17:10:40 -0700

    WIP. TwColorsGrid. update PreviewBox

--- src/components/sections/Section3_Tailwind/TwColorInfo.tsx
+++ src/components/sections/Section3_Tailwind/TwColorInfo.tsx
@@ -8,6 +8,7 @@ import useMeasure from 'react-use-measure';
 
 function PreviewBox() {
     const currentTwColor = useAtomValue(currentTwColorAtom);
+    const txtColor = { color: currentTwColor ? isLightColor(currentTwColor.value) ? 'black' : 'white' : '' };
     return (
         <div
             className="flex-none w-24 h-16 text-xs border-primary-400 border rounded flex transition-colors"
@@ -15,10 +16,10 @@ function PreviewBox() {
         >
             {currentTwColor &&
                 <div className="flex-1 px-1.5 py-0.5 flex items-end justify-between">
-                    <div className="transition-colors" style={{ color: isLightColor(currentTwColor.value) ? "black" : "white" }}>
+                    <div className="transition-colors" style={txtColor}>
                         {currentTwColor.group}
                     </div>
-                    <div className="transition-colors" style={{ color: isLightColor(currentTwColor.value) ? "black" : "white" }}>
+                    <div className="transition-colors" style={txtColor}>
                         {currentTwColor.key}
                     </div>
                 </div>
@@ -43,13 +44,13 @@ function RowPalette({ groupName, className }: { groupName: string; } & HTMLAttri
     const values = Object.entries(allColors[groupName]);
     const [currentTwColor, setCurrentTwColor] = useAtom(currentTwColorAtom);
     return (
-        <div className={classNames("grid justify-end text-xs select-none", className)} style={{gridTemplateColumns: `repeat(${groupCnt}, minmax(0,1fr))`}}>
+        <div className={classNames("grid justify-end text-xs select-none", className)} style={{ gridTemplateColumns: `repeat(${groupCnt}, minmax(0,1fr))` }}>
             {values.map(([key, color], idx) => (
                 <div
                     className={classNames(
                         "relative pr-1 min-w-[2rem] h-16 cursor-pointer flex items-end justify-end",
                         currentTwColor?.value === color &&
-                            "after:absolute after:left-0 after:-bottom-1.5 after:w-full after:h-1 after:bg-slate-400/40 after:border-slate-400 after:border-t",
+                        "after:absolute after:left-0 after:-bottom-1.5 after:w-full after:h-1 after:bg-slate-400/40 after:border-slate-400 after:border-t",
                     )}
                     style={{ backgroundColor: color }}
                     onClick={() => setCurrentTwColor((v) => v && { group: v.group, key, value: color })}
